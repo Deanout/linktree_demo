6dd18cfa4f657977e1626f188235493f326567cc
Dean DeHart <DeHartDean@gmail.com>
2023-09-18 16:31:42 -0400

    Add Admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'admin#index'
+  get 'admin/appearance'
+  get 'admin/analytics'
+  get 'admin/settings'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "articles#index"
