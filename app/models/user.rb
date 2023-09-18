12419e7e026a54aa7ef11967231c1599d34d429c
Dean DeHart <DeHartDean@gmail.com>
2023-09-18 19:30:38 -0400

    Add usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  normalizes :username, with: ->(username) { username.downcase }
+  normalizes :email, with: ->(email) { email.strip.downcase }
+
+  # Only allow letters and numbers in username, because we use it in URLs
+  validates :username, 
+    presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    format: { with: /\A[a-zA-Z0-9]+\z/ }, 
+    length: { minimum: 3, maximum: 20 }
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value', { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
