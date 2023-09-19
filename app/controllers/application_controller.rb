820b010d3085e17de0877b540128c028456f21e1
Dean DeHart <DeHartDean@gmail.com>
2023-09-19 13:18:58 -0400

    Add Display Names To Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[username email password password_confirmation remember_me avatar]
+    added_attrs = %i[username email password password_confirmation remember_me avatar name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
---
+++ db/migrate/20230919171630_add_full_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddFullNameToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :name, :string
+  end
+end
